de694d301e3d41eee9b3fa430b92ed1a9613fe3d
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-15 14:27:16 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,18 @@
+# collection = (1..100).to_a
+# my_find(collection) {|i| i % 3 == 0 && i % 5 == 0 }
+
 require 'pry'
 
 def my_find(collection)
+  new_array = []
+  i = 0
+
+  while i < collection.length
+    new_array << yeild(collection[i])
+    i += 1
+  end
+binding.pry 
+  if new_array.include?(f)
+
 
 end
