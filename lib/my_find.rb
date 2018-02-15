31de122459dbfaceae37601e488041c5cd8f51d8
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-15 14:40:39 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -6,7 +6,8 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    return yield(collection[i]) if yield(collection[i])
+    return collection[i] if yield(collection[i])
     i += 1
   end
 end
+
